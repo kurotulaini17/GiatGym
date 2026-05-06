e391e577983fb8f98d338e32e76595f0c0cbd694
Siti Kurotul Aini <kurotulaini17@gmail.com>
2026-05-07 06:49:43 +0700

    tambah fitur timer countdown

--- lib/timer_page.dart
+++ lib/timer_page.dart
@@ -1,15 +1,49 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class TimerPage extends StatelessWidget {
+class TimerPage extends StatefulWidget {
   final String nama;
+
   const TimerPage({super.key, required this.nama});
+
+  @override
+  State<TimerPage> createState() => _TimerPageState();
+}
+
+class _TimerPageState extends State<TimerPage> {
+  int waktu = 10;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(const Duration(seconds: 1), (t) {
+      if (waktu > 0) {
+        setState(() {
+          waktu--;
+        });
+      } else {
+        t.cancel();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(nama)),
+      appBar: AppBar(title: Text(widget.nama)),
       body: Center(
-        child: Text("Halaman $nama", style: const TextStyle(fontSize: 24)),
+        child: Text(
+          "$waktu",
+          style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
