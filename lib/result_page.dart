2cd553f0142f33b5645ff341ac8d499e02deaf75
Siti Kurotul Aini <kurotulaini17@gmail.com>
2026-05-09 09:38:13 +0700

    upgrade tampilan result page

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -9,20 +9,47 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Selesai")),
-      body: Center(
+      body: Container(
+        width: double.infinity,
+
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.orangeAccent, Colors.white],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Icon(Icons.check_circle, size: 120, color: Colors.green),
+
+            const SizedBox(height: 30),
+
             Text(
               "$nama selesai 💪🏻",
-              style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 20),
+
+            const SizedBox(height: 30),
+
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 15,
+                ),
+              ),
+
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: const Text("Kembali"),
+
+              child: const Text(
+                "Kembali",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
             ),
           ],
         ),
